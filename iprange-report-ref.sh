2245939453ffdf4592543b02b7d4b13e7889d897
puppet-staging <servicedesk@devellar.com>
2017-04-10 22:08:33 -0400

    Add script which collect info about all EC2,SG

---
+++ iprange-report-ref.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#Check available EC2 instances in AWS Regions
+
+Purple='\033[0;35m'
+White='\033[0;37m'        
+Green='\033[0;32m'    
+
+LISTREGIONS=`aws ec2 describe-regions --query 'Regions[*].RegionName' --output text`
+INDEX=0
+for region in $LISTREGIONS;
+do
+                EC2ID=`aws ec2 describe-instances --region $region --query 'Reservations[*].Instances[*].InstanceId' --output text`
+                if [[ -z "${EC2ID// }" ]]; then
+                        echo -e $White"Looking Instances in region: "$region...$EC2ID
+                else
+                        echo -e $Green ++++++ Found EC2 in region: $region +++++++
+                        GROUPID=`aws ec2 describe-instances  --region $region --instance-ids $EC2ID --query 'Reservations[*].Instances[*].SecurityGroups[*].GroupId'  --output text`
+                        echo -e $White =================================
+                        echo -e $Purple REGION '=>' $region;
+                        for group in $GROUPID;do
+                                echo -e $Purple "    SG => $group"
+                                echo -e $White "FROM   TO      IP"
+                                aws ec2 describe-security-groups --region $region  --group-ids $group --query "SecurityGroups[*].IpPermissions[*].[FromPort,ToPort,join(',',IpRanges[*].CidrIp)]"  --output text
+                                RULES[$INDEX]=`aws ec2 describe-security-groups --region $region  --group-ids $group --query "SecurityGroups[*].IpPermissions[*].[FromPort,ToPort,join(',',IpRanges[*].CidrIp)]"  --output text | awk '{print$3}'| sort -u | uniq`         
+                                let INDEX=${INDEX}+1;
+                                echo "";
+                        done
+                fi
+done
+
+for ip in ${RULES[@]};do
+        IP=`echo $ip | cut -d\/ -f1`
+        ORG=`whois $IP | grep 'OrgName'`
+        echo -e $Green $IP $Purple '=>' $White $ORG
+done
