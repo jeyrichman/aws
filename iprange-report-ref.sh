be78800d3f7ca341d303e63265d2d43da38756c4
puppet-staging <servicedesk@devellar.com>
2017-04-12 14:39:11 -0400

    fix color

--- iprange-report-ref.sh
+++ iprange-report-ref.sh
@@ -3,7 +3,8 @@
 
 Purple='\033[0;35m'
 White='\033[0;37m'        
-Green='\033[0;32m'    
+Green='\033[0;32m'   
+Yellow='\033[93m'
 
 LISTREGIONS=`aws ec2 describe-regions --query 'Regions[*].RegionName' --output text`
 INDEX=0
@@ -13,7 +14,7 @@ do
                 if [[ -z "${EC2ID// }" ]]; then
                         echo -e $White"Looking Instances in region: "$region...$EC2ID
                 else
-                        echo -e $Green ++++++ Found EC2 in region: $region +++++++
+                        echo -e $Green ++++++ Found EC2 in region: $region:$Yellow$EC2ID
                         GROUPID=`aws ec2 describe-instances  --region $region --instance-ids $EC2ID --query 'Reservations[*].Instances[*].SecurityGroups[*].GroupId'  --output text`
                         echo -e $White =================================
                         echo -e $Purple REGION '=>' $region;
